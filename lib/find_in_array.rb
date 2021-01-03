da2903f8d17e9fe4f11f5f4cbf8d39f2c8d3af5a
Alexandra Peccia <ampeccia@gmail.com>
2021-01-03 23:54:22 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,11 +1,9 @@
-
-
 def find_element_index(array, value_to_find)
   count = 0 
   not_value = []
   
   while count < array.length do 
-    if array[count] != value_to_find
+    if array[count] != value_to_find 
       not_value.push(array[count])
       count +=1 
         if not_value.length == array.length 
