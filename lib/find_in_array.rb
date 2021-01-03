6e2a97bd39aabf6b52f8e29993a9ae9410f413cd
Alexandra Peccia <ampeccia@gmail.com>
2021-01-03 23:51:47 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,21 @@
+
+
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
-end
+  count = 0 
+  not_value = []
+  
+  while count < array.length do 
+    if array[count] != value_to_find
+      not_value.push(array[count])
+      count +=1 
+        if not_value.length == array.length 
+          "nil" 
+        end 
+    elsif array[count] == value_to_find
+      index = count 
+      break 
+    end 
+  end 
+  index
+end 
+
